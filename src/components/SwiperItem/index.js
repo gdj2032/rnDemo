e4dd4828634fed36b54f829fe0cda24f379da77b
djgu <djgu@tmindtech.com>
2019-08-07 10:21:09 +0800

    fix

--- src/components/SwiperItem/index.js
+++ src/components/SwiperItem/index.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Swiper from 'react-native-swiper';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Swiper from "react-native-swiper";
+import {themesColor} from '../../style';
 
 export default class SwiperItem extends Component {
-
   render() {
     return (
       <Swiper
@@ -12,15 +12,30 @@ export default class SwiperItem extends Component {
         height={120}
         autoplay={true}
         autoplayTimeout={5}
+        paginationStyle={{ bottom: 10 }} //小圆点的位置：距离底部10p
+        dot={//未选中的圆点样式
+          <View
+            style={[{
+              backgroundColor: themesColor.white,
+            }, styles.dot_style]}
+          />
+        }
+        activeDot={//选中的圆点样式
+          <View
+            style={[{
+              backgroundColor: themesColor.red,
+            }, styles.dot_style]}
+          />
+        }
       >
         <View style={styles.slide1}>
-            <Text style={styles.text}>Hello Swiper</Text>
+          <Text style={styles.text}>Hello Swiper</Text>
         </View>
         <View style={styles.slide2}>
-            <Text style={styles.text}>Beautiful</Text>
+          <Text style={styles.text}>Beautiful</Text>
         </View>
         <View style={styles.slide3}>
-            <Text style={styles.text}>And simple</Text>
+          <Text style={styles.text}>And simple</Text>
         </View>
       </Swiper>
     );
@@ -28,32 +43,38 @@ export default class SwiperItem extends Component {
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-  },
+  wrapper: {},
   slide1: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#9DD6EB',
-      borderRadius: 20,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#9DD6EB",
+    borderRadius: 20
   },
   slide2: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#97CAE5',
-      borderRadius: 20,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#97CAE5",
+    borderRadius: 20
   },
   slide3: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#92BBD9',
-      borderRadius: 20,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#92BBD9",
+    borderRadius: 20
   },
   text: {
-      color: '#fff',
-      fontSize: 30,
-      fontWeight: 'bold'
+    color: "#fff",
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  dot_style: {
+    width: 6,
+    height: 6,
+    borderRadius: 6,
+    marginLeft: 4,
+    marginRight: 4,
   }
 });
--- src/style/index.js
+++ src/style/index.js
@@ -5,4 +5,5 @@ export const themesColor = {
   backgroundColor: '#FFF',
   white: '#FFFFFF',
   black: '#000',
+  red: '#FF3030',
 }
