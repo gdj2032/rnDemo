396b01e30fd5838a19a28b9120da745818b08206
djgu <djgu@tmindtech.com>
2019-08-09 14:30:21 +0800

    add pullscroll 下拉刷新

--- src/pages/Test/index.js
+++ src/pages/Test/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, PanResponder, Animated} from 'react-native';
 import { Button, Icon } from '@ant-design/react-native';
 import { themesColor } from '../../style';
-import PullToRefresh from '../../components/PullToRefresh';
+import PullScrollView from '../../components/PullScrollView';
 
 export default class Test extends Component {
   static navigationOptions = {
@@ -11,20 +11,34 @@ export default class Test extends Component {
     // },
   };
 
-  _onRefresh = () => {
-    console.log('onRefresh');
+  state = {
+  }
+  onPullRelease(resolve) {
+    //刷新完毕，重置下拉刷新，再次更新刷新和加载更多状态
+    console.log('onPullRelease')
+    setTimeout(() => {
+      resolve();
+    }, 3000);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <PullToRefresh
-          onRefresh={this._onRefresh.bind(this)}
+        <PullScrollView
+          style={{flex: 1, backgroundColor: 'white'}}
+          onPullRelease={this.onPullRelease}
         >
           <Text>11111</Text>
           <Text>2222</Text>
           <Text>44444</Text>
-        </PullToRefresh>
+          <Text>11111</Text>
+          <Text>2222</Text>
+          <Text>44444</Text>
+          <Text>11111</Text>
+          <Text>2222</Text>
+          <Text>44444</Text>
+          <Text>11111</Text>
+        </PullScrollView>
       </View>
     );
   }
